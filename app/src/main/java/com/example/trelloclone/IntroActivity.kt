39b6d770b2bf1b940a8aeba322027adba2a36b81
AkinchanKushwaha <akinchandroger@gmail.com>
2021-02-28 14:27:49 +0530

    add SignIn Activity

--- app/src/main/java/com/example/trelloclone/IntroActivity.kt
+++ app/src/main/java/com/example/trelloclone/IntroActivity.kt
@@ -10,7 +10,9 @@ class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
-
+        btn_sign_in_intro.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
         btn_sign_up_intro.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
         }
---
+++ app/src/main/java/com/example/trelloclone/SignInActivity.kt
@@ -0,0 +1,28 @@
+package com.example.trelloclone
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.trelloclone.R
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+
+        setSupportActionBar(toolbar_sign_in_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+
+        toolbar_sign_in_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+}
