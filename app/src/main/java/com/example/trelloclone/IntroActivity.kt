c3472ae3e4e3d8c940761b4ab0fff6e0874be3e3
AkinchanKushwaha <akinchandroger@gmail.com>
2021-02-28 13:31:02 +0530

    add SignUp Activity

--- app/src/main/java/com/example/trelloclone/IntroActivity.kt
+++ app/src/main/java/com/example/trelloclone/IntroActivity.kt
@@ -1,11 +1,19 @@
 package com.example.trelloclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_intro.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        btn_sign_up_intro.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
     }
 }
