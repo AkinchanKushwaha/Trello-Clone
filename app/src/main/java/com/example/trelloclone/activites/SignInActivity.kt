55a2896f7061dc9545cb275ec981ea34dda65bd5
AkinchanKushwaha <akinchandroger@gmail.com>
2021-03-04 13:33:03 +0530

    implemented the functions to Sign In using firebase.

--- app/src/main/java/com/example/trelloclone/activites/SignInActivity.kt
+++ app/src/main/java/com/example/trelloclone/activites/SignInActivity.kt
@@ -1,16 +1,29 @@
 package com.example.trelloclone.activites
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.trelloclone.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
         setupActionBar()
+
+        auth = FirebaseAuth.getInstance()
+
+        btn_sign_in.setOnClickListener {
+            signInRegisteredUser()
+        }
     }
 
     private fun setupActionBar() {
@@ -25,4 +38,46 @@ class SignInActivity : AppCompatActivity() {
 
         toolbar_sign_in_activity.setNavigationOnClickListener { onBackPressed() }
     }
+
+    private fun signInRegisteredUser(){
+        val email: String = et_email_signIn.text.toString().trim{ it <= ' '}
+        val password : String = et_password_signIn.text.toString().trim{ it <= ' '}
+
+        if(validateForm(email, password)){
+            showProgressDialog(resources.getString(R.string.please_wait))
+            auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) {task ->
+                        hideProgressDialog()
+                        if(task.isSuccessful){
+                            Log.d(TAG, "signInWithEmail:success")
+                            val user = auth.currentUser
+                            startActivity(Intent(this@SignInActivity,MainActivity::class.java )  )
+                        }else{
+                            Log.w(TAG, "signInWithEmail:failure", task.exception)
+                            Toast.makeText(this@SignInActivity, "Authentication Failed", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+        }
+
+    }
+
+    private fun validateForm( email : String, password : String ): Boolean{
+        return when{
+            TextUtils.isEmpty(email)->{
+                showErrorSnackBar("Please enter an email address")
+                false
+            }
+            TextUtils.isEmpty(password)->{
+                showErrorSnackBar("Please enter a password")
+                false
+            }else -> true
+        }
+    }
+
+    companion object{
+        private const val TAG = "SignInActivity"
+    }
+
+
 }
