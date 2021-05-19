ebe49f5807b9e7c9f32e2bedc95622ff989d12d1
AkinchanKushwaha <akinchandroger@gmail.com>
2021-05-19 12:01:11 +0530

    Implemented the logic of adding a new member to board.

--- app/src/main/java/com/example/trelloclone/activities/MembersActivity.kt
+++ app/src/main/java/com/example/trelloclone/activities/MembersActivity.kt
@@ -20,6 +20,7 @@ import kotlinx.android.synthetic.main.dialog_search_member.*
 class MembersActivity : BaseActivity() {
 
     private lateinit var mBoardDetails: Board
+    private lateinit var mAssignedMembersList: ArrayList<User>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,12 +49,18 @@ class MembersActivity : BaseActivity() {
     }
 
     fun setUpMembersList(list: ArrayList<User>){
+        mAssignedMembersList = list
         hideProgressDialog()
         rv_members_list.layoutManager = LinearLayoutManager(this)
         rv_members_list.setHasFixedSize(true)
         val adapter = MemberListItemsAdapter(this, list)
         rv_members_list.adapter = adapter
 
+    }
+
+    fun memberDetails(user: User){
+        mBoardDetails.assignedTo.add(user.id)
+        FirestoreClass().assignMemberToBoard(this, mBoardDetails, user)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -78,7 +85,8 @@ class MembersActivity : BaseActivity() {
             val email = dialog.et_email_search_member.text.toString()
             if(email.isNotEmpty()){
                 dialog.dismiss()
-                //TODO implement adding member logic
+                showProgressDialog(resources.getString(R.string.please_wait))
+                FirestoreClass().getMemberDetails(this, email)
             }else{
                 Toast.makeText(this , "Please enter an email address.", Toast.LENGTH_SHORT).show()
             }
@@ -87,5 +95,11 @@ class MembersActivity : BaseActivity() {
             dialog.dismiss()
         }
         dialog.show()
+    }
+
+    fun memberAssignSuccess(user: User){
+        hideProgressDialog()
+        mAssignedMembersList.add(user)
+        setUpMembersList(mAssignedMembersList)
     }
 }
