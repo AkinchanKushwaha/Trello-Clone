fb433a36ce200f323d38a013f6b7d42c655e8ab6
AkinchanKushwaha <akinchandroger@gmail.com>
2021-05-24 21:35:52 +0530

    added color selecting dialog and Adapter.

--- app/src/main/java/com/example/trelloclone/activities/CardDetailsActivity.kt
+++ app/src/main/java/com/example/trelloclone/activities/CardDetailsActivity.kt
@@ -2,12 +2,14 @@ package com.example.trelloclone.activities
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.trelloclone.R
+import com.example.trelloclone.dialogs.LabelColorListDialog
 import com.example.trelloclone.firebase.FirestoreClass
 import com.example.trelloclone.models.Board
 import com.example.trelloclone.models.Card
@@ -20,6 +22,7 @@ class CardDetailsActivity : BaseActivity() {
     private lateinit var mBoardDetails: Board
     private var mTaskListPosition = -1
     private var mCardPosition = -1
+    private var mSelectedColor = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +40,10 @@ class CardDetailsActivity : BaseActivity() {
                 Toast.makeText(this, "Please Enter a Card Name", Toast.LENGTH_SHORT).show()
             }
         }
+
+        tv_select_label_color.setOnClickListener{
+            labelColorsListDialog()
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -52,6 +59,41 @@ class CardDetailsActivity : BaseActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_delete_card, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun colorsList(): ArrayList<String> {
+
+        val colorsList: ArrayList<String> = ArrayList()
+        colorsList.add("#43C86F")
+        colorsList.add("#0C90F1")
+        colorsList.add("#F72400")
+        colorsList.add("#7A8089")
+        colorsList.add("#D57C1D")
+        colorsList.add("#770000")
+        colorsList.add("#0022F8")
+
+        return colorsList
+    }
+
+    private fun setColor(){
+        tv_select_label_color.text = ""
+        tv_select_label_color.setBackgroundColor(Color.parseColor(mSelectedColor))
+    }
+
+    private fun labelColorsListDialog(){
+        val colorsList: ArrayList<String> = colorsList()
+
+        val listDialog = object : LabelColorListDialog(
+            this,
+            colorsList,
+            resources.getString(R.string.str_select_label_color)
+        ){
+            override fun onItemSelected(color: String) {
+                mSelectedColor = color
+                setColor()
+            }
+        }
+        listDialog.show()
     }
 
 
